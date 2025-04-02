33aff70546c6ec01fb0fd604507b46ce956397f2
PRANAV K DILEEP <91619846+pranavkdileep@users.noreply.github.com>
2025-04-02 17:35:59 +0000

    implemented ws

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const puppeteer = require('puppeteer');
 const fs = require('fs');
-const imageValid = require('valid-jpeg');
 const axios = require('axios');
-const FormData = require('form-data');
+const WebSocket = require('ws');
 
 require('dotenv').config();
 
 const app = express();
+const url = 'wss://google-sdxl.hf.space/queue/join';
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.post('/generate', async (req, res) => {
     const { prompt, negativePrompt } = req.body;
@@ -19,82 +17,78 @@ app.post('/generate', async (req, res) => {
     res.send(Json);
 });
 
-async function genimage(prompt, negativePrompt) {
-    const browser = await puppeteer.launch({
-        executablePath: "/usr/bin/google-chrome",
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        headless: 'new'
-    });
-    const page = await browser.newPage();
-    await page.setRequestInterception(true);
+async function genimage(prompt, negativePrompt = "", guidancescale = 7.5, style = "(No style)") {
+    const sesionhash = generateSessionHash();
+    return new Promise((resolve, reject) => {
+        const ws = new WebSocket(url);
+        ws.on('open', () => {
+            console.log('Connected to WebSocket server');
+        });
 
-    page.on('request', (request) => {
-        request.continue();
-    });
-
-    var filename = generateRandomFilename('png');
-    page.on('response', async (response) => {
-        if (response.url().includes('data:image')) {
-            fs.writeFileSync("images/" + filename, await response.buffer());
-        }
-    });
-    try{
-    await page.goto('https://google-sdxl.hf.space');
-    await page.setViewport({ width: 1480, height: 1100 });
-    await page.waitForSelector('input[placeholder="Enter your prompt"]');
-    await page.type('input[placeholder="Enter your prompt"]', prompt);
-    await new Promise(r => setTimeout(r, 3000));
-    await page.click('#component-11 > button');
-    await page.waitForSelector('input[placeholder="Enter a negative prompt"]');
-    await page.type('input[placeholder="Enter a negative prompt"]', negativePrompt);
-    await page.screenshot({ path: 'examplebutton.png' });
-    await page.waitForSelector('button[id="gen-button"]');
-    await page.click('button[id="gen-button"]');
-    await new Promise(r => setTimeout(r, 10000));
-    await page.waitForSelector('#gallery > div.grid-wrap.svelte-vg7mqq.fixed-height > div > div > button');
-    await page.click('#gallery > div.grid-wrap.svelte-vg7mqq.fixed-height > div > div > button');
-    await new Promise(r => setTimeout(r, 5000));
-    await page.screenshot({ path: 'example.png' });
-    await browser.close();
-    }catch(e){
-        console.log(e);
-    }
-    
-
-    return new Promise((resolve) => {
-        imageValid.isValid("images/" + filename, (err, valid) => {
-            if (err) {
-                console.log(err);
-                resolve(JSON.stringify({ valid: false, error: err }));
+        ws.on('message', (data) => {
+            console.log('Received:', data.toString());
+            const message = JSON.parse(data.toString());
+            if (message.msg === 'send_hash') {
+                ws.send(JSON.stringify({ "fn_index": 3, "session_hash": sesionhash }));
+                console.log({ "fn_index": 3, "session_hash": sesionhash })
             }
-            if (valid) {
-                let api_key = process.env.IMAGE_API_KEY;
-                let filePath = "images/" + filename;
-                let form = new FormData();
-                form.append('image', fs.createReadStream(filePath));
-                axios.post(`https://api.imgbb.com/1/upload?expiration=600&key=${api_key}`, form, {
-                    headers: form.getHeaders()
-                })
-                .then(response => {
-                    console.log(response.data);
-                    let data = JSON.stringify(response.data);
-                    let image_url = JSON.parse(data).data.url;
-                    console.log(image_url);
-                    resolve(JSON.stringify({ valid: true, ImageUrl: image_url }));
-                })
-                .catch(error => {
-                    resolve(JSON.stringify({ valid: false, error: error }));
-                });
-            } else {
-                resolve(JSON.stringify({ valid: false, error: "invalid image" }));
+            if (message.msg === 'queue_full') {
+                console.log('Queue full');
+                ws.close();
+                reject('Queue full');
             }
+            if (message.msg === 'send_data') {
+                const data = { "data": [prompt, negativePrompt, guidancescale, style], "event_data": null, "fn_index": 3, "session_hash": sesionhash };
+                ws.send(JSON.stringify(data));
+                console.log(data);
+            }
+            if (message.msg === 'process_completed') {
+                if(message.success){
+                    const images = message.output.data[0];
+                    images.forEach((image, index) => {
+                        const filePath = `./images/image_${index}.jpg`;
+                        //saveBase64Image(image, filePath);
+                    });
+                }else{
+                    reject(message.output.error);
+                }
+            }
+        });
+
+        ws.on('close', () => {
+            console.log('WebSocket connection closed');
+        });
+
+        ws.on('error', (err) => {
+            console.error('WebSocket error:', err);
         });
     });
 }
 
-function generateRandomFilename(extension) {
-    return Math.random().toString(36).substring(2, 15) + Date.now().toString() + '.' + extension;
+function generateSessionHash(length = 10) {
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
 }
 
-const port = 3000;
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+function saveBase64Image(base64Data, filePath) {
+    const base64String = base64Data.replace(/^data:image\/jpeg;base64,/, '');
+    const imageBuffer = Buffer.from(base64String, 'base64');
+    fs.writeFile(filePath, imageBuffer, (err) => {
+        if (err) {
+            console.error('Error saving image:', err);
+        } else {
+            console.log('Image saved successfully:', filePath);
+        }
+    });
+}
+
+
+// const port = 3000;
+// app.listen(port, () => console.log(`Server listening on port ${port}`));
+
+
+genimage("A serious capybara at work, wearing a suit").then(console.log).catch(console.error);
